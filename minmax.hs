a0d7964a097f0bcfb90d0cd56704620387f1e39e
Daniel Gustafsson <daniel.gustafsson@gmail.com>
2013-11-25 14:07:38 +0100

    update the plot

--- minmax.hs
+++ minmax.hs
@@ -1,11 +1,14 @@
 {-# LANGUAGE OverloadedStrings #-}
 import Control.Monad
 import Data.Monoid
-import qualified Data.Map as Map
+import Data.Function
+import Data.List
+import qualified Data.HashMap.Strict as Map
 import qualified Data.Text as T
 import qualified Data.Text.IO as T
 
-data P = P !T.Text !T.Text
+data P = P {minP , maxP :: !T.Text}
+
 
 minMax (P x y) (P z t) = P (x `min` z) (y `max` t)
 
@@ -15,6 +18,7 @@ interactT f = T.putStr . f =<< T.getContents
 
 main = interactT $ T.unlines
                  . concatMap showEntry
+                 . sortBy (compare `on` minP . snd)
                  . Map.toList
                  . Map.fromListWith minMax
                  . map ((\[x,y] -> (y,P x x)) . T.words)
--- splot.sh
+++ splot.sh
@@ -1,1 +1,1 @@
-splot -tf '%H:%M:%S' -if inp.1000000  -o out.png
+splot -tickInterval 60000 -w 1920 -h 1080 -tf '%H:%M:%S' -if $1  -o out.png
