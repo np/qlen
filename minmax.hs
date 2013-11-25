a3b721b55c3ec3b48fcea603b58e48fcae2f35ac
Nicolas Pouillard <nicolas.pouillard@gmail.com>
2013-11-25 13:06:46 +0100

    switch from string to text

--- minmax.hs
+++ minmax.hs
@@ -1,13 +1,21 @@
+{-# LANGUAGE OverloadedStrings #-}
+import Control.Monad
+import Data.Monoid
 import qualified Data.Map as Map
+import qualified Data.Text as T
+import qualified Data.Text.IO as T
 
-data P = P !String !String
+data P = P !T.Text !T.Text
 
 minMax (P x y) (P z t) = P (x `min` z) (y `max` t)
 
-showEntry (x,P y z) = [y ++ " >" ++ x, z ++ " <" ++ x]
+showEntry (x,P y z) = [y <> " >" <> x, z <> " <" <> x]
 
-main = interact $ unlines . concatMap showEntry
-                          . Map.toList
-                          . Map.fromListWith minMax
-                          . map ((\[x,y] -> (y,P x x)) . words)
-                          . lines
+interactT f = T.putStr . f =<< T.getContents
+
+main = interactT $ T.unlines
+                 . concatMap showEntry
+                 . Map.toList
+                 . Map.fromListWith minMax
+                 . map ((\[x,y] -> (y,P x x)) . T.words)
+                 . T.lines
